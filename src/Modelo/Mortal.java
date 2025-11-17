f4c6b3bb97f07d65a323e510b685b96cd0743216
matheusgaubeur <matheus.gaubeur@gmail.com>
2025-11-17 20:15:45 -0300

    Update Mortal.java

--- src/Modelo/Mortal.java
+++ src/Modelo/Mortal.java
@@ -2,5 +2,6 @@ package Modelo;
 
 //Interface para marcar Personagens que causam a morte do Herói ao colidir.
 public interface Mortal {
-    // É uma interface marcadora não precisa de métodos.
+ 
+
 }
